b8235a1e257fcc97181f12cf4f3c3e40af5c1438
papapingouin-be <82971316+papapingouin-be@users.noreply.github.com>
2025-09-23 23:56:41 +0200

    Display service statuses on OLED at boot

--- src/OledPin.h
+++ src/OledPin.h
@@ -11,6 +11,8 @@
 namespace OledPin {
   /** Initialise l'écran OLED (SSD1306 128x64 I2C). */
   void begin();
+  /** Affiche des informations de statut (WiFi, services) pendant le boot. */
+  void showStatus(const String& wifi, const String& web, const String& udp);
   /** Affiche le code PIN à l'écran. */
   void showPIN(int pin);
 }
